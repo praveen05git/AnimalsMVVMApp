74af02e48be5f200257ce6145268359375542f28
praveen05git <praveenmail@protonmail.com>
2020-11-10 12:03:54 +0530

    Initial commit

---
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.hencesimplified.mvvmretrofitsample.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.hencesimplified.mvvmretrofitsample.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listButton.setOnClickListener(View.OnClickListener {
+            val action : NavDirections = DetailFragmentDirections.actionList()
+            Navigation.findNavController(it).navigate(action)
+        })
+
+    }
+
+
+}
---
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.hencesimplified.mvvmretrofitsample.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.hencesimplified.mvvmretrofitsample.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //App bar with back button
+        navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
