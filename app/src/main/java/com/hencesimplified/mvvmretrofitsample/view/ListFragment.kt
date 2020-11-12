1a71173f9e3044ae01a5e65d8111a8ff2e41f296
praveen05git <praveenmail@protonmail.com>
2020-11-12 14:45:59 +0530

    Implemented Retrofit api service

--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/ListFragment.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/ListFragment.kt
@@ -59,6 +59,14 @@ class ListFragment : Fragment() {
             layoutManager = GridLayoutManager(context, 2)
             adapter = listAdapter
         }
+
+        refreshLayout.setOnRefreshListener {
+            animalList.visibility = View.GONE
+            listError.visibility = View.GONE
+            loadingView.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
     }
 
 
