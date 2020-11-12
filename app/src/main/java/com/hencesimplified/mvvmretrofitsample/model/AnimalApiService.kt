378b82e6cdaae85127261f13faacec178ad31d0f
praveen05git <praveenmail@protonmail.com>
2020-11-12 17:15:29 +0530

    Established communication with the API

--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/model/AnimalApiService.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/model/AnimalApiService.kt
@@ -8,18 +8,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 class AnimalApiService {
 
     private val BASE_URL = "https://us-central1-apis-4674e.cloudfunctions.net"
-    private val api= Retrofit.Builder()
+    private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(AnimalApi::class.java)
 
-    fun getApiKey(): Single<ApiKey>{
+    fun getApiKey(): Single<ApiKey> {
         return api.getApiKey()
     }
 
-    fun getAnimals(key: String): Single<List<Animal>>{
+    fun getAnimals(key: String): Single<List<Animal>> {
         return api.getAnimals(key)
     }
 
--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/viewmodel/ListViewModel.kt
@@ -4,6 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.hencesimplified.mvvmretrofitsample.model.Animal
+import com.hencesimplified.mvvmretrofitsample.model.AnimalApiService
+import com.hencesimplified.mvvmretrofitsample.model.ApiKey
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -11,10 +17,64 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
     val loadError by lazy { MutableLiveData<Boolean>() }
     val loading by lazy { MutableLiveData<Boolean>() }
 
+    private val disposable = CompositeDisposable()
+    private val apiService = AnimalApiService()
+
     fun refresh() {
-        getAnimals()
+        loading.value = true
+        getKey()
     }
 
+    private fun getKey() {
+        disposable.add(
+            apiService.getApiKey()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<ApiKey>() {
+                    override fun onSuccess(apiKey: ApiKey) {
+                        if (apiKey.key.isNullOrEmpty()) {
+                            loadError.value = true
+                            loading.value = false
+                        } else {
+                            getAnimals(apiKey.key)
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        loadError.value = true
+                        loading.value = false
+                    }
+
+                })
+        )
+    }
+
+    private fun getAnimals(key: String) {
+        disposable.add(
+            apiService.getAnimals(key)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Animal>>() {
+                    override fun onSuccess(animalList: List<Animal>) {
+                        animals.value = animalList
+                        loadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        animals.value = null
+                        loadError.value = true
+                        loading.value = false
+                    }
+
+                })
+        )
+    }
+
+    /*
+    //Local hardcoded data
     private fun getAnimals() {
         val a1 = Animal("Dog")
         val a2 = Animal("Cat")
@@ -25,4 +85,5 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
         loadError.value = false
         loading.value = false
     }
+     */
 }
