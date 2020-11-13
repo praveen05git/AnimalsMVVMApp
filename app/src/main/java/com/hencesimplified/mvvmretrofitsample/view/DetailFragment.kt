11d85a6972702d370374bbf1f4f91c49fb557301
praveen05git <praveenmail@protonmail.com>
2020-11-13 17:01:15 +0530

    Added color Palette

--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/DetailFragment.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/DetailFragment.kt
@@ -1,10 +1,16 @@
 package com.hencesimplified.mvvmretrofitsample.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.hencesimplified.mvvmretrofitsample.R
 import com.hencesimplified.mvvmretrofitsample.model.Animal
 import com.hencesimplified.mvvmretrofitsample.util.getProgressDrawable
@@ -31,14 +37,37 @@ class DetailFragment : Fragment() {
         }
 
         context?.let {
-            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
+            animalDetailImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
         }
 
-        animalName.text = animal?.name
-        animalLocation.text = animal?.location
-        animalLifespan.text = animal?.lifeSpan
-        animalDiet.text = animal?.diet
+        animalDetailName.text = animal?.name
+        animalDetailLocation.text = animal?.location
+        animalDetailLifespan.text = animal?.lifeSpan
+        animalDetailDiet.text = animal?.diet
 
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            animalDetailLayout.setBackgroundColor(intColor)
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+            })
     }
 
 }
