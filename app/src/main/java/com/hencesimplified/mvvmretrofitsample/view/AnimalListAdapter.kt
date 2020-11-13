202e1b811f34359532e29c2bfd8a2d6432671373
praveen05git <praveenmail@protonmail.com>
2020-11-13 10:45:56 +0530

    Implemented Glide for image processing from URL

--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/AnimalListAdapter.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/AnimalListAdapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hencesimplified.mvvmretrofitsample.R
 import com.hencesimplified.mvvmretrofitsample.model.Animal
+import com.hencesimplified.mvvmretrofitsample.util.getProgressDrawable
+import com.hencesimplified.mvvmretrofitsample.util.loadImage
 import kotlinx.android.synthetic.main.item_animal.view.*
 
 class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
@@ -27,6 +29,10 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
         holder.view.animalName.text = animalList[position].name
+        holder.view.animalImage.loadImage(
+            animalList[position].imageUrl,
+            getProgressDrawable(holder.view.context)
+        )
     }
 
     override fun getItemCount() = animalList.size
