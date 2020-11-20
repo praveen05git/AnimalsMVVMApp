302b714aeb04bf9ffbd1c81f91dfa74e93a2a3a7
praveen05git <praveenmail@protonmail.com>
2020-11-20 11:45:52 +0530

    Added data binding for DetailFragment.kt

--- app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/DetailFragment.kt
+++ app/src/main/java/com/hencesimplified/mvvmretrofitsample/view/DetailFragment.kt
@@ -6,27 +6,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.hencesimplified.mvvmretrofitsample.R
+import com.hencesimplified.mvvmretrofitsample.databinding.FragmentDetailBinding
 import com.hencesimplified.mvvmretrofitsample.model.Animal
-import com.hencesimplified.mvvmretrofitsample.util.getProgressDrawable
-import com.hencesimplified.mvvmretrofitsample.util.loadImage
-import kotlinx.android.synthetic.main.fragment_detail.*
+import com.hencesimplified.mvvmretrofitsample.model.AnimalPalette
 
 class DetailFragment : Fragment() {
 
     var animal: Animal? = null
+    private lateinit var dataBinding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        //return inflater.inflate(R.layout.fragment_detail, container, false) Implemented Data binding
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,18 +40,26 @@ class DetailFragment : Fragment() {
             animal = DetailFragmentArgs.fromBundle(it).animal
         }
 
+/*
         context?.let {
-            animalDetailImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
+            dataBinding.animalDetailImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
         }
+*/
 
+        /*
+        Data Binding has been implemented
         animalDetailName.text = animal?.name
         animalDetailLocation.text = animal?.location
         animalDetailLifespan.text = animal?.lifeSpan
         animalDetailDiet.text = animal?.diet
+         */
 
         animal?.imageUrl?.let {
             setupBackgroundColor(it)
         }
+
+        dataBinding.animal = animal
+
 
     }
 
@@ -60,7 +72,8 @@ class DetailFragment : Fragment() {
                     Palette.from(resource)
                         .generate() { palette ->
                             val intColor = palette?.lightMutedSwatch?.rgb ?: 0
-                            animalDetailLayout.setBackgroundColor(intColor)
+                            //dataBinding.animalDetailLayout.setBackgroundColor(intColor) Databinding
+                            dataBinding.palette = AnimalPalette(intColor)
                         }
                 }
 
